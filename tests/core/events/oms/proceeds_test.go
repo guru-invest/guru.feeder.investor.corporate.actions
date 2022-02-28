bb4b7d8040d2189f6366186850bda1cd16b20b44
Tadashi <victor.t.y.z@hotmail.com>
2022-02-28 09:49:48 -0300

    adicionado testes de proventuos bonificação

--- tests/core/events/oms/proceeds_test.go
+++ tests/core/events/oms/proceeds_test.go
@@ -38,7 +38,7 @@ func newCorporateActionObject(description string, value float64, com_date time.T
 	return result
 }
 
-func TestProceeds(t *testing.T) {
+func TestProceedsCash(t *testing.T) {
 
 	// Dados chave para identificação dos ativos por usuários
 	Customer := "fzVzgo8b"
@@ -98,6 +98,68 @@ func TestProceeds(t *testing.T) {
 	}
 
 	ExpectedProceedsEvent := "JRS CAP PROPRIO"
+	if OMSProceedPersisterObject[0].Event != ExpectedProceedsEvent {
+		t.Errorf("TestProceeds Amount: Esperado (%s), Recebido (%s)", ExpectedProceedsEvent, OMSProceedPersisterObject[0].Event)
+	}
+}
+
+func TestProceedsBonus(t *testing.T) {
+
+	// Dados chave para identificação dos ativos por usuários
+	Customer := "2prEs4ie"
+
+	// Mock de transações
+	OMSTransactionObjects := []mapper.OMSTransaction{}
+	OMSTransactionObjects = append(OMSTransactionObjects, newTransactionObject("ITSA4", 17, 11.69, StrToDate("2021-06-11 11:42:43"), 1618))
+	OMSTransactionObjects = append(OMSTransactionObjects, newTransactionObject("ITSA4", 6, 11.69, StrToDate("2021-06-11 11:42:43"), 1618))
+	OMSTransactionObjects = append(OMSTransactionObjects, newTransactionObject("ITSA4", 20, 11.69, StrToDate("2021-06-11 11:42:43"), 1618))
+	// Agrupamento das transações em um map com chave por usuário
+	OMSTransactions := map[string][]mapper.OMSTransaction{}
+	OMSTransactions[Customer] = OMSTransactionObjects
+
+	// Mock de eventos corporativos
+	CorporateActionObjects := []mapper.CorporateAction{}
+	CorporateActionObjects = append(CorporateActionObjects, newCorporateActionObject("BONIFICACAO", 5, StrToDate("2021-12-20 0:00:00")))
+	// Agrupamento dos eventos corporativos em um map com chave por symbol
+	CorporateActions := map[string][]mapper.CorporateAction{}
+	CorporateActions["ITSA4"] = CorporateActionObjects
+
+	// Processamento
+	OMSProceedPersisterObject := []mapper.OMSProceeds{}
+	OMSProceedPersisterObject = append(OMSProceedPersisterObject, oms.ApplyProceedsCorporateAction(Customer, "ITSA4", OMSTransactions, CorporateActions)...)
+
+	// Teste
+	ExpectedProceedsCount := 1
+	if len(OMSProceedPersisterObject) != ExpectedProceedsCount {
+		t.Errorf("TestProceeds Count: Esperado (%d), Recebido (%d)", ExpectedProceedsCount, len(OMSProceedPersisterObject))
+	}
+
+	ExpectedProceedsSymbol := "ITSA4"
+	if OMSProceedPersisterObject[0].Symbol != ExpectedProceedsSymbol {
+		t.Errorf("TestProceeds Symbol: Esperado (%s), Recebido (%s)", ExpectedProceedsSymbol, OMSProceedPersisterObject[0].Symbol)
+	}
+
+	ExpectedProceedsQuantity := 43
+	if int(OMSProceedPersisterObject[0].Quantity) != ExpectedProceedsQuantity {
+		t.Errorf("TestProceeds Quantity: Esperado (%d), Recebido (%d)", ExpectedProceedsQuantity, int(OMSProceedPersisterObject[0].Quantity))
+	}
+
+	ExpectedProceedsValue := 5.0
+	if OMSProceedPersisterObject[0].Value != ExpectedProceedsValue {
+		t.Errorf("TestProceeds Value: Esperado (%f), Recebido (%f)", ExpectedProceedsValue, OMSProceedPersisterObject[0].Value)
+	}
+
+	ExpectedProceedsAmount := 8.0
+	if OMSProceedPersisterObject[0].Amount != ExpectedProceedsAmount {
+		t.Errorf("TestProceeds Amount: Esperado (%f), Recebido (%f)", ExpectedProceedsAmount, OMSProceedPersisterObject[0].Amount)
+	}
+
+	ExpectedProceedsComDate := StrToDate("2021-12-20 0:00:00")
+	if OMSProceedPersisterObject[0].Date != ExpectedProceedsComDate {
+		t.Errorf("TestProceeds Amount: Esperado (%s), Recebido (%s)", ExpectedProceedsComDate.String(), OMSProceedPersisterObject[0].Date.String())
+	}
+
+	ExpectedProceedsEvent := "BONIFICACAO"
 	if OMSProceedPersisterObject[0].Event != ExpectedProceedsEvent {
 		t.Errorf("TestProceeds Amount: Esperado (%s), Recebido (%s)", ExpectedProceedsEvent, OMSProceedPersisterObject[0].Event)
 	}
