be477f1c9e5ae7e0d17d4ac7d755391482b6549e
Tiago Sanches <tisanches@gmail.com>
2022-08-12 14:18:43 -0300

    tratamento para target ticker, quando muda o nome do ticker esse campo vem vazio para eventos antigos

--- src/core/events/cei/basic.go
+++ src/core/events/cei/basic.go
@@ -9,13 +9,18 @@ import (
 // Eventos basicos contemplam Grupamento, Desdobramento e Atualização
 func ApplyBasicCorporateAction(CEITransaction mapper.CEITransaction, corporate_action mapper.CorporateAction) mapper.CEITransaction {
 
+	targetTicket := corporate_action.TargetTicker
+	if corporate_action.TargetTicker == "" {
+		targetTicket = corporate_action.Symbol
+	}
+
 	CEITransaction.EventFactor = corporate_action.CalculatedFactor
 	// Quando for um evento de Atualização, o Fator deve ser 1, pois a Quantidade e o Preço não podem ser alterados.
 	if corporate_action.Description == constants.Update {
 		CEITransaction.EventFactor = 1
 	}
 	CEITransaction.EventName = corporate_action.Description
-	CEITransaction.PostEventSymbol = corporate_action.TargetTicker
+	CEITransaction.PostEventSymbol = targetTicket
 	CEITransaction.EventDate = corporate_action.ComDate
 	CEITransaction.PostEventQuantity = float64(CEITransaction.Quantity) / CEITransaction.EventFactor
 	CEITransaction.PostEventPrice = utils.Truncate(CEITransaction.Amount/CEITransaction.PostEventQuantity, 2)
