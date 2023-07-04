bc1bb6cdd5a9e4ce94346065f5ad112f62fa8f3a
tisanches <tisanches@gmail.com>
2023-07-04 17:36:50 -0300

    acerta executor

--- cmd/guru.feeder.investor.corporate.actions.go
+++ cmd/guru.feeder.investor.corporate.actions.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/guru-invest/guru.feeder.investor.corporate.actions/src/constants"
 	"github.com/guru-invest/guru.feeder.investor.corporate.actions/src/core"
 	"github.com/guru-invest/guru.feeder.investor.corporate.actions/src/crossCutting/options"
+	"github.com/sirupsen/logrus"
 )
 
 func init() {
@@ -14,21 +19,23 @@ func init() {
 func main() {
 	//time_zone, _ := time.LoadLocation("America/Sao_Paulo")
 	fmt.Println("Fluxo Iniciado")
-	core.ApplyEvents("fzVzgo8b")
+	//core.ApplyEvents("fzVzgo8b")
+	core.ApplyEvents(constants.AllCustomers)
 	//core.ApplyEventsAfterInvestorSync("fzVzgo8b")
 	defer func() {
 		if r := recover(); r != nil {
-			fmt.Printf("panic: %v \n", r)
+			logrus.WithFields(logrus.Fields{
+				"Service": "guru.feeder.investor.corporate.actions",
+				"Caller":  "main",
+				"Error":   r,
+			}).Error("Erro inexperado no fluxo - [Panic]")
 
 		}
 	}()
 	//core.Run()
 	fmt.Println("Fluxo Finalizado")
-	// c := cron.New(cron.WithLocation(time_zone))
-	// c.AddFunc("30 2 * * *", func() { core.Run() })
-	// go c.Start()
-	// sig := make(chan os.Signal, 1)
-	// signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
 
-	// <-sig
+	listener := make(chan os.Signal, 1)
+	signal.Notify(listener, os.Interrupt, syscall.SIGTERM)
+	<-listener
 }
